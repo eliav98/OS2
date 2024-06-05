7b76f64b09bd03cd67291e640076dd9a0ebd3444
Eliav <eliavdayanof@gmail.com>
2024-06-05 15:16:16 +0300

    put enum in public

--- src/Thread.h
+++ src/Thread.h
@@ -2,17 +2,19 @@
 // Created by noga on 02-Jun-24.
 //
 
-enum Mode {RUNNING, READY, BLOCKED};
 
 #ifndef _THREAD_H_
 #define _THREAD_H_
 
-class Thread
-{
-    int tid;
-    Mode mode;
-    int num_quantums;
-};
+class ThreadDescriptor {
+ public:
+  enum Mode {
+      RUNNING, READY, BLOCKED
+  };
 
+  int tid;
+  Mode mode;
+  int num_quantums;
+};
 
 #endif //_THREAD_H_
